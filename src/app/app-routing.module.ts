1944010a09e02b38e56feda29801095f5eeceb5d
elmoamedeo <elmoamedeo@gmail.com>
2019-05-14 12:47:37 -0300

    zzz

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PainelComponent } from "./painel/painel.component";
+import { PessoasComponent } from "./pessoas/pessoas.component"
+import { PessoaDetalheComponent } from "./pessoa-detalhe/pessoa-detalhe.component";
+
+const routes: Routes = [
+  { path: '', redirectTo: '/painel', pathMatch: 'full' },
+  { path: 'painel', component: PainelComponent },
+  { path: 'detalhe/:id', component: PessoaDetalheComponent },
+  { path: 'pessoas', component: PessoasComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { PessoasComponent } from "./pessoas/pessoas.component";
+import { PessoaDetalheComponent } from './pessoa-detalhe/pessoa-detalhe.component';
+import { MensagensComponent } from './mensagens/mensagens.component';
+import { PainelComponent } from './painel/painel.component';
+import { PessoaPesquisaComponent } from './pessoa-pesquisa/pessoa-pesquisa.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+
+    // O módulo HttpClientInMemoryWebApiModule intercepta solicitações HTTP
+    // e retorna respostas do servidor simuladas.
+    // Remover quando um servidor real estiver pronto para receber solicitações.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    PessoasComponent,
+    PessoaDetalheComponent,
+    MensagensComponent,
+    PainelComponent,
+    PessoaPesquisaComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
